1f447e171b796b0f078e4722b03fbdb70af65348
Cohen <1768065921@qq.com>
2024-03-28 09:45:21 +0800

    添加UI,添加router

--- applications/dedsi.blogger.portal/src/main.ts
+++ applications/dedsi.blogger.portal/src/main.ts
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// UI
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+
+// router
+import router from './router/index'
+
+const vueApp = createApp(App);
+
+vueApp
+.use(Antd)
+.use(router)
+.mount('#app')
